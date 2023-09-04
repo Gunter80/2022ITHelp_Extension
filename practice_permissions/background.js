2e64c7d1f758f60f8030ed46a826f17f944f44aa
gunter <g24966362@gmail.com>
2023-09-04 08:08:17 +0800

    登入路徑順了

--- practice_permissions/background.js
+++ practice_permissions/background.js
@@ -1,5 +1,21 @@
 let notificationId ;
 
+function sendtopopup(params) {
+  chrome.runtime.sendMessage({ message: params});
+}
+
+function sendMsgToDb(val){
+  const dbemail = 'g24966362@gmail.com';
+  const dbpass = 'enovel800312';
+  if(val.email === dbemail && dbpass === val.password){
+    console.log("DB login success");
+    return true;
+  }else{
+    console.log("DB login fail");
+    return false;
+  }
+}
+
 // 當網站中的頁面做更新的時候觸發
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete" ){
@@ -23,6 +39,54 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
       )
     }
   }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === 'login') {
+    // email = chrome.storage.sync.get(["email"]).then((result) => {
+    //   console.log("Value currently is " + result.email);
+    // });
+    // pass = chrome.storage.sync.get(["pass"]).then((result) => {
+    //   console.log("Value currently is " + result.pass);
+    // });
+    console.log(request.payload.email);
+    DBback = sendMsgToDb(request.payload);
+    if(DBback){
+      chrome.storage.sync.set({ email: request.payload.email }).then(() => {
+        console.log("email is set");
+      });
+      chrome.storage.sync.set({ pass: request.payload.password }).then(() => {
+        console.log("pass is set");
+      });
+    }else{
+      sendtopopup("登入失敗");
+    }
+
+
+    // if(request.payload.email === email && request.payload.password === pass){
+    //   chrome.storage.local.set({ userStatus: signIn, user_info }, function (response) {
+    //     if (chrome.runtime.lastError) resolve('fail');
+
+    //     user_signed_in = signIn;
+    //     resolve('success');
+    // });
+
+    // }
+
+
+    //if storage same to login message
+
+    //send login message to server
+    
+    //send message to popup.js
+
+      return true;
+  } else if (request.message === 'logout') {
+      flip_user_status(false, null)
+          .then(res => sendResponse(res))
+          .catch(err => console.log(err));
+      return true;
+  } 
 });
 
 
--- practice_permissions/popup/popup.js
+++ practice_permissions/popup/popup.js
@@ -1,8 +1,9 @@
 const btn = document.querySelector('.btn');
+const dologin = document.querySelector('#sendlogin');
 
-const injectWelcomItem =(src) => { 
-  const div = document.createElement('div').innerHTML=
-  `
+const injectWelcomItem = (src) => {
+  const div = document.createElement('div').innerHTML =
+    `
   <img style="width:100%; height:100%;"
   src="${src}"
   > `
@@ -15,10 +16,24 @@ const getStorage = async (key) => {
   })
 };
 
-btn.addEventListener('click', async () => { 
-  const isURLMatch =await getStorage('isURLMatch');
+btn.addEventListener('click', async () => {
+  const isURLMatch = await getStorage('isURLMatch');
 
-  if(isURLMatch){
+  if (isURLMatch) {
     console.log(isURLMatch);
   }
+});
+
+dologin.addEventListener('click', async () => {
+  email = document.querySelector('#email').value;
+  password = document.querySelector('#pass').value;
+  chrome.runtime.sendMessage({ message: 'login', payload: { email, password } }, function (response) {
+
+  });
 })
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === '登入失敗') {
+    alert("登入失敗");
+  }
+});
